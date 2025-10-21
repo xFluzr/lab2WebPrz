ec03bcb1a003494bfb8f2423df8d4cf05c480602
Kuba <jakub.jaszcz538@gmail.com>
2025-10-21 09:26:47 +0200

    Ulepszony loading 2_1.2.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 (function () {
-  const example = document.getElementById('example')
-  const cw1 = document.getElementById('cw1')
-  const cw2 = document.getElementById('cw2')
-  const cw3 = document.getElementById('cw3')
-  const answer = document.getElementById('answer')
+  const example = document.getElementById("example");
+  const cw1 = document.getElementById("cw1");
+  const cw2 = document.getElementById("cw2");
+  const cw3 = document.getElementById("cw3");
+  const answer = document.getElementById("answer");
 
   example.addEventListener("click", function () {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(array => {
-        console.log(array)
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((array) => {
+        console.log(array);
         answer.innerHTML = JSON.stringify(array);
-      })
-  })
+      });
+  });
 
   let styledHtml = "";
   cw1.addEventListener("click", function () {
@@ -30,7 +30,7 @@
         });
         answer.innerHTML = styledHtml;
       });
-  })
+  });
 
   let loadingElement = `<div><h2>Loading...</h2></div>`;
   cw2.addEventListener("click", async function () {
@@ -38,19 +38,29 @@
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const responseArray = await res.json();
     let arrayStyled = ``;
-    responseArray.map((post) => {
-      console.log(post);
-      arrayStyled += `<div class='post'>
-          <h4>${post.title}</h4>
-          <p>${post.body}</p>
+    setTimeout(() => {
+      responseArray.map((post) => {
+        console.log(post);
+        arrayStyled += `<div class='post'>
+          <h4 class='header-post'>${post.title}</h4>
+          <p class='post-body'>${post.body}</p>
           <p>Numer posta:<span class='postNumber'>${post.id}</span></p>
           </div>`;
-    });
-    answer.innerHTML = arrayStyled;
+      });
+      answer.innerHTML = arrayStyled;
+    }, 1000);
   });
 
-  cw3.addEventListener("click", function () {
-    //TODO
-  })
-
+  styledHtml = "";
+  cw3.addEventListener("click", async function () {
+    answer.innerHTML = loadingElement;
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const resData = await res.json();
+    let arrayStyled = `<div class='post'>
+          <h4>${resData.title}</h4>
+          <p>${resData.body}</p>
+          <p>Numer posta:<span class='postNumber'>${resData.id}</span></p>
+          </div>`;
+    answer.innerHTML = arrayStyled;
+  });
 })();
